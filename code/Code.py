5bb18dd1e45406f064b9954fe3f2a3e34f7b37b6
Akhil Soman <58958652+neo047@users.noreply.github.com>
2020-06-08 02:14:23 -0400

    Update Code.py

--- code/Code.py
+++ code/Code.py
@@ -23,7 +23,7 @@ from keras.layers import Dense
 from keras.callbacks import EarlyStopping
 from keras import regularizers
 from keras.optimizers import Adam
-#for saving to csv
+from keras.wrappers.scikit_learn import KerasRegressor
 from os import listdir
 import csv
 import pandas
@@ -107,59 +107,56 @@ def traintest(data,lend = 7008, n_fea = 40, n_lookback = 48, n_lookahead = 24):
     return train_X, train_y, test_X, test_y
 
 # STEP 2
-# The function below calls the processed dataset and trains the neural network. The 
+# The function below calls the processed dataset and trains the neural network. the function nn_model defines the LSTM model
+# based on user settings. 
 
-def NN(Neurons = 100, Neuron2 = 125, Neuron3 = 125, Neuron4 = 100, epochs = 50,batchsize = 512,
-       breg = 0.01, dropout = 0.1, layer = 2, lr = 0.001, n_lookahead = 24,recurrent = 'hard_sigmoid'):
-    global Pickled_save, train_X,train_y,test_y,test_X
-    n_lookahead = 24
-    fea = '40_Features'
-    RS = False
-    f_bias = 0
-    bs = 1
-    print('Started')
-    print(Neurons,Neuron2,Neuron3,Neuron4,batchsize,breg,dropout,layer,n_lookahead)
+def nn_model(Neuron1, Neuron2, Neuron3, Neuron4, epochs, batchsize,
+       breg, dropout, layer, n_lookahead ,recurrent):
     #
     model=Sequential()
     if layer == 1:
-        model.add(LSTM(Neurons,recurrent_activation = 'hard_sigmoid', input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
+        model.add(LSTM(Neuron1,recurrent_activation = 'hard_sigmoid', input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
                dropout = dropout, return_sequences=False, unit_forget_bias = 1))
     elif layer == 2:
-        model.add(LSTM(Neurons,input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
+        model.add(LSTM(Neuron1,input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
                dropout = dropout, return_sequences=True,unit_forget_bias = 1))
         model.add(LSTM(Neuron2))
     elif layer == 3:
-        model.add(LSTM(Neurons,input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
+        model.add(LSTM(Neuron1,input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
                 return_sequences=True,unit_forget_bias = 1))
         model.add(LSTM(Neuron2, return_sequences = True,dropout = dropout))
         model.add(LSTM(Neuron3,dropout = dropout))
     elif layer == 4:
-        model.add(LSTM(Neurons,input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
-                return_sequences=True,recurrent_activation = recurrent, return_state = RS,unit_forget_bias = f_bias, use_bias = bs))
-        model.add(LSTM(Neuron2, dropout = dropout, return_sequences = True,recurrent_activation = recurrent,return_state = RS,unit_forget_bias = f_bias, use_bias = bs))
-        model.add(LSTM(Neuron3, dropout = dropout, return_sequences = True,recurrent_activation = recurrent,return_state = RS,unit_forget_bias = f_bias, use_bias = bs))
-        model.add(LSTM(Neuron4,recurrent_activation = recurrent, use_bias = bs))
+        model.add(LSTM(Neuron1,input_shape=(train_X.shape[1], train_X.shape[2]),kernel_initializer='glorot_uniform', bias_regularizer = regularizers.l2(breg),
+                return_sequences=True,recurrent_activation = recurrent,unit_forget_bias = 0, use_bias = 1))
+        model.add(LSTM(Neuron2, dropout = dropout, return_sequences = True,recurrent_activation = recurrent,unit_forget_bias = 0, use_bias = 1))
+        model.add(LSTM(Neuron3, dropout = dropout, return_sequences = True,recurrent_activation = recurrent,unit_forget_bias = 0, use_bias = 1))
+        model.add(LSTM(Neuron4,recurrent_activation = recurrent, use_bias = 1))
     model.add(Dense(n_lookahead))
-    adam = Adam(lr = lr,decay = 0.001)
+    adam = Adam(lr = 0.1,decay = 0.001)
     model.compile(loss='mean_squared_error', optimizer = adam)
-    start = time.time()
-    history = model.fit(train_X, train_y, epochs=epochs, batch_size=batchsize,
-            validation_split = 0.1, verbose=1, shuffle=True)  
-    end = time.time()
-    print('The time elapsed for 10 epochs in seconds is', end-start)
-    yhat = model.predict(test_X)
-    #
-    model.get_config()
-    k = model.get_weights()
-    model.summary() 
-    GG = "Model.pickle"
-    pickle_out = open(GG,"wb")
-    pickle.dump(k,pickle_out)
-    #
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    #
-    return yhat, loss, val_loss
+    return model
+
+# Once the model is defined, the hyperparameters of the neural network can be tuned in the user defined grid space using the
+# code below. The number of layers, number of neurons, regularizer and dropout are the hyperparameters tuned and its grid 
+# space is defined by the values in between a user defined range at regular steps. This means that the user defines the range
+# of neurons by definining (Nstart, Nstop, Nstep) in the code below. Nstart = min value of range, Nstop = Max value of range, 
+# Nstep = steps/interval length to generate the values between start and stop. The same translate to regularizer (Bstart, Bstop,
+# Bstep) and dropout (Dstart, Dstop, Dstep).
+
+def gridsearch(Nstart,Nstop,Nstep,Bstart, Bstop, Bstep,Dstart, Dstop, Dstep, layer):
+    Neuron1 = np.arange(Nstart,Nstop,Nstep)
+    Neuron2 = np.arange(Nstart,Nstop,Nstep)
+    Neuron3 = np.arange(Nstart,Nstop,Nstep)
+    Neuron4 = np.arange(Nstart,Nstop,Nstep)
+    breg = np.arange(Bstart, Bstop, Bstep)
+    dropout = np.arange(Dstart, Dstop, Dstep)
+    reg = KerasRegressor(build_fn = nn_model, epochs = 100, verbose = 1, batchsize = 72, layer = layer, n_lookahead = 24, recurrent = 'hard_sigmoid')
+    param_grid = dict(Neuron1 = Neuron1, Neuron2 = Neuron2,
+                      Neuron3 = Neuron3, Neuron4 = Neuron4, breg = breg, dropout = dropout)
+    grid = GridSearchCV(reg, param_grid=param_grid, n_jobs=-1)
+    grid_result = grid.fit(train_X, train_y)
+    return grid_result
 
 
 train_X = np.load('trainX.npy')
